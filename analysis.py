860f9f3fdad1091ecd9ddbc14c3a3e75656f215b
sebastian-ml <sebastian2549@wp.pl>
2023-02-01 22:34:01 +0100

    Change bar chart code

--- analysis.py
+++ analysis.py
@@ -4,19 +4,19 @@ from datasets import altcoins_bought, btc_data
 
 plt.style.use('seaborn')
 
-
 # Coins included in analysis
 altcoins_table = altcoins_bought[['coin_name', 'buy', 'date']]
 print(altcoins_table)
 
 coins_by_buy_group = altcoins_table['buy'].value_counts()
-largest_group_count = max(coins_by_buy_group)
 
-coins_by_buy_group.plot(kind='bar',
-                        title='Number of coins in each group')
+fig_1, ax_1 = plt.subplots()
 
-plt.xticks(rotation='horizontal')
-plt.yticks(np.arange(0, largest_group_count + 1, 1))
+ax_1.bar(coins_by_buy_group.index,
+         coins_by_buy_group.values)
+
+ax_1.xaxis.set_major_locator(plt.MaxNLocator(integer=True))
+ax_1.set_title('Number of coins in each group')
 
 # Bitcoin
 buy_dates = altcoins_table[altcoins_table['buy'] > 0]
@@ -29,20 +29,20 @@ btc_prices_on_buy = buy_dates.merge(
     right_on='date'
 )
 
-fig, ax = plt.subplots()
+fig_2, ax_2 = plt.subplots()
 
-ax.plot(btc_data['date'],
-        btc_data['price'],
-        zorder=1)
+ax_2.plot(btc_data['date'],
+          btc_data['price'],
+          zorder=1)
 
-ax.scatter(btc_prices_on_buy['date'],
-           btc_prices_on_buy['price'],
-           s=30,
-           c='r',
-           zorder=2)
+ax_2.scatter(btc_prices_on_buy['date'],
+             btc_prices_on_buy['price'],
+             s=30,
+             c='r',
+             zorder=2)
 
-ax.xaxis.set_major_locator(plt.MaxNLocator(7))
-ax.tick_params(axis='x', labelrotation=45)
-ax.set_title("Bitcoin's price history")
+ax_2.xaxis.set_major_locator(plt.MaxNLocator(7))
+ax_2.tick_params(axis='x', labelrotation=45)
+ax_2.set_title("Bitcoin's price history")
 
 plt.show()
